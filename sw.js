b9254a3cd4331f1ed635afdb642d2087390fe07b
danielszu <127829546+danielszu@users.noreply.github.com>
2026-05-07 15:01:54 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'lbr-pwa-v1';
+const CACHE_NAME = 'lbr-pwa-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './icon-192v2.png',
+  './icon-512v2.png',
   'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;600&family=Noto+Serif+TC:wght@400;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
